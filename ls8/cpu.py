748a2e72c0d459f40deb72e2081f0b8a89268525
cdixson-ds <crystal.dixson@gmail.com>
2020-10-14 22:28:40 -0700

    Add files via upload

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,11 +2,14 @@
 
 import sys
 
-LDI = 0b10000010
-PRN = 0b01000111
-HLT = 0b00000001
+SP = 7
+LDI = 0b10000010  #load immediate, store a value in register, or 'set this register to this value'
+PRN = 0b01000111  #a pseudo-instruction that prints the numeric value stored in a register
+HLT = 0b00000001  #halt the CPU and exit the emulator
 MUL = 0b10100010
 ADD = 0b10100000
+PUSH = 0b01000101
+POP = 0b01000110
 
 class CPU:
     """Main CPU class."""
@@ -16,72 +19,68 @@ class CPU:
         self.ram = [0] * 256  #memory
         self.reg = [0] * 8    #registers
         self.pc = 0           #program counter
-        self.running = True
+        # self.running = True
         
 
-    def ram_read(self, address):
-        return self.ram[address]
+    def ram_read(self, address):    #should accept the address to read and return the value stored there
+        if address < len(self.ram):
+            return self.ram[address]
+        else:
+            return None
 
-    def ram_write(self, value, address):
+    def ram_write(self, value, address):    #should accept a value to write and address to write it to
         self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
-        address = 0
+        
 
         if len(sys.argv) < 2:
             print('please include a filename in the second argument')
             sys.exit()
         try:
-            with open(sys.argv[1]) as f:
-                for line in f:
-                    split_file = line.split('#')
+            address = 0
+            with open(sys.argv[1]) as filename:
+                for line in filename:
+                    split_file = line.strip().split('#')
                     val = split_file[0].strip()
                     if val == "":
                         continue
 
-                    try:
-                        instruction = int(val, 2)
-                    except ValueError:
-                        print(f"Invalid number {n}")
-                        sys.exit(1)
-
+                    instruction = int(val, 2)
                     self.ram[address] = instruction
                     address += 1
                     
-        except FileNotFoundError:
-            print(f"{sys.argv[0]} / {sys.argv[1]} file not found")
+        except:
+            print("file not found")
             sys.exit()
 
 
-
-        # address = 0
-        # #For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
-
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == "SUB":
-            self.reg[reg_a] -= self.reg[reg_b]
+        # elif op == "SUB":
+        #     self.reg[reg_a] -= self.reg[reg_b]
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+
+        # elif op == PUSH:
+        #     self.reg[SP] -= 1
+        #     reg_num = self.ram_read(self.pc+1)
+        #     val = self.reg[reg_num]
+        #     add_top = self.reg[SP]
+        #     self.ram[add_top] = val
+        #     self.pc += 2
+
+        # elif op == POP:
+
+        #     reg_num = self.ram_read(self.pc + 1)
+        #     add_top = self.reg[SP]
+        #     val = self.ram_read[add_top]
+        #     self.reg[reg_num] = val
+        #     self.reg[SP] += 1
+        #     self.pc += 2
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -108,11 +107,13 @@ class CPU:
     def run(self):
         """Run the CPU."""
         self.running = True
-        
+       
+ 
         while self.running:
             ir = self.ram_read(self.pc)  
             op_a = self.ram_read(self.pc+1)
             op_b = self.ram_read(self.pc+2)
+            str_ir = str(ir)
 
             if ir == HLT: 
                 self.running = False
@@ -128,10 +129,28 @@ class CPU:
                 self.pc += 3
             
             elif ir == MUL:
-                result = self.reg[op_a] + self.reg[op_b]
+                #self.alu(ir, op_a, op_b)
+                result = self.reg[op_a] * self.reg[op_b]
                 print(result)
                 self.pc += 3
+
+            elif ir == PUSH:
+                self.reg[SP] -= 1
+                reg_num = self.ram_read(self.pc+1)
+                value = self.reg[reg_num]
+                add_top = self.reg[SP]
+                self.ram[add_top] = value
+                self.pc += 2
+
+            elif ir == POP:
+                reg_num = self.ram_read(self.pc + 1)
+                add_top = self.reg[SP]
+                value = self.ram_read(add_top)
+                self.reg[reg_num] = value
+                self.reg[SP] += 1
+                self.pc += 2
+
             else:
-                self.running = False
                 print(f"Invalid entry: {ir}")
+                sys.exit()
 
